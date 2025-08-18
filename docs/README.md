693f18eda07cabe4ecdad1ddea8923381c6505a9
Habutiii <56110862+Habutiii@users.noreply.github.com>
2025-08-18 19:42:11 +0800

    Rename project to HaBot. Added Greet and Bye
    (a) Changed bot name to HaBot
    (b) Implemented an initial skeletal version

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# HaBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/HaBot.java
@@ -0,0 +1,44 @@
+public class HaBot {
+    // Define the name and logo of the bot
+    private static final String NAME = "HaBot";
+
+    // Scanner for reading user input
+    private static final java.util.Scanner SCANNER = new java.util.Scanner(System.in);
+
+    private static final String SEPARATOR = "-".repeat(50);
+    private static void printSeparator() {
+        System.out.println(SEPARATOR);
+    }
+
+    private static void send(String message) {
+        printSeparator();
+        System.out.println("<|°_°|>");
+        System.out.println(message);
+        printSeparator();
+    }
+
+    private static void greet() {
+        String logo = """
+                 _     _           ____ \s
+                 |_____|  ____    |____]   ____  __|__  \s
+                 |     | |____|__ |_____] |____|   |__  \s
+                """;
+        System.out.println(logo);
+        send("Hello! I'm " + NAME + "!\nWhat can I do for you?");
+    }
+
+    private static void bye() {
+        send("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+
+        String endpoint = "bye";
+        // Print the greeting message
+        greet();
+
+        // Print the goodbye message
+        bye();
+
+    }
+}
