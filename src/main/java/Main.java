9db1189de93ef2cb4eff308f1d680a1216825aa9
Habutiii <56110862+Habutiii@users.noreply.github.com>
2025-08-29 20:03:00 +0800

    Add Automatic Resize Elements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,10 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+
             fxmlLoader.<MainWindow>getController().setBot(habot);  // inject the HaBot instance
             stage.show();
         } catch (IOException e) {
